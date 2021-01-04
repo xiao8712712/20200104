13c423587da4fb14b960055d73633bb484929cce
xiao8712712 <1141381181@qq.com>
2021-01-04 21:43:41 +0800

    xiaoli

--- SqlServer/src/com/xiao/sqlservertest/CeShi.java
+++
@@ -1,9 +0,0 @@
-package com.xiao.sqlservertest;
-
-public class CeShi {
-	
-	public static void main(String[] args) {
-		System.out.println("xiaoliceshigit11111111111111111111111111111111111111111111111111111111111111111111111");
-	}
-
-}
